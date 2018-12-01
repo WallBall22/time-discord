0167dac2bc294996f7f94ea097f64e87f238b61e
WallBall22 <44549704+WallBall22@users.noreply.github.com>
2018-12-01 21:25:28 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,44 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+var prefix = "*";
+client.on('message', message => {
+        if (message.content === prefix + "dt") {
+            if (!message.channel.guild) return message.reply('** This command only for servers **');  
+var currentTime = new Date(),
+            hours = currentTime.getHours() + 0 ,
+            minutes = currentTime.getMinutes(),
+            seconds = currentTime.getSeconds();
+            Year = currentTime.getFullYear(),
+            Month = currentTime.getMonth() + 1,
+            Day = currentTime.getDate();
+
+            if (minutes < 10) {
+                minutes = '0' + minutes;
+            }
+            var suffix = 'صباحاَ';
+            if (hours >= 12) {
+                suffix = 'مساء';
+                hours = hours - 12;
+            }
+            if (hours == 0) {
+                hours = 12;
+            }
+
+
+                var Date15= new Discord.RichEmbed()
+                .setThumbnail(message.author.avatarURL) 
+                .setTitle("**الوقت وتاريخ**")
+                .setColor('RANDOM')
+                .setTimestamp()
+                .addField('Time',
+                "『"+ hours + ":" + minutes + "』") 
+                .addField('Date',
+                "『"+ Day + "-" + Month + "-" + Year + "』")
+
+                 message.channel.sendEmbed(Date15);
+        }
+    });
+
+
+
+  client.login(process.env.BOT_TOKEN);
